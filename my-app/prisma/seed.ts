13527617a9e8fc4c933f0d9be88ccbfb8ba3e072
Axelle-LM <98803198+Axelle-LM@users.noreply.github.com>
2026-07-08 16:27:30 +0200

    image redimension

--- my-app/prisma/seed.ts
+++ my-app/prisma/seed.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  
+
   await prisma.projet.createMany({
     data: [
       {
@@ -13,7 +13,7 @@ async function main() {
         technologies: "React, Node.js, MongoDB.",
         resultats: "Augmentation des ventes de 30%.",
         objectifs: "Créer une expérience utilisateur fluide et intuitive.",
-        images:"https://i.postimg.cc/hvjbDLVQ/portfolio1.png",
+        images: "https://i.postimg.cc/8zZPkMZs/1.png",
         videos: "https://drive.google.com/file/d/10uNfAKtuvBrGO1wCkRpKt1rV6eMENhGL/view?usp=drive_link",
         testimonials: "Bonne expérience, site rapide et efficace.",
         client: "Client D",
@@ -28,14 +28,14 @@ async function main() {
         technologies: "Flutter, Firebase.",
         resultats: "Amélioration de la productivité des utilisateurs.",
         objectifs: "Permettre un suivi efficace des tâches.",
-        images: "https://i.postimg.cc/yWq44XGs/app.png",
+        images: "https://i.postimg.cc/13v5Rwvn/2.png",
         videos: "https://drive.google.com/file/d/182SGwU2F2uCV8WMr1GDn5LNRk1vPWVHa/view?usp=drive_link",
         testimonials: "L'application a révolutionné notre organisation quotidienne.",
         client: "Client E",
         date_realisation: new Date("2025-01-15"),
         completed: true,
       },
-         
+
       {
         name: "Système de réservation",
         description: "Une application de réservation en ligne.",
@@ -43,7 +43,7 @@ async function main() {
         technologies: "Laravel, Vue.js, MySQL.",
         resultats: "Diminution du taux d'annulation des rendez-vous.",
         objectifs: "Simplifier la gestion des réservations pour les clients.",
-        images:"https://i.postimg.cc/tgGTtFC0/syst-me-reservation.png",
+        images: "https://i.postimg.cc/7ZKYP0K2/3.png",
         videos: "https://drive.google.com/file/d/10uNfAKtuvBrGO1wCkRpKt1rV6eMENhGL/view?usp=drive_link",
         testimonials: "Réservation simple et rapide, interface agréable.",
         client: "Client H",
@@ -53,17 +53,17 @@ async function main() {
 
       {
         name: "Site vitrine",
-      description: "Un site vitrine pour une entreprise.",
-      contexte: "Présenter les services et produits d'une société.",
-      technologies: "WordPress, Elementor.",
-      resultats: "Augmentation des demandes de contact.",
-      objectifs: "Offrir une vitrine moderne et responsive.",
-      images:"https://i.postimg.cc/fyr7JVv2/mediath-que.png",
-      videos: "https://drive.google.com/file/d/182SGwU2F2uCV8WMr1GDn5LNRk1vPWVHa/view?usp=drive_link",
-      testimonials: "Un site clair et efficace pour notre entreprise.",
-      client: "Client I",
-      date_realisation: new Date("2024-02-10"),
-      completed: true,
+        description: "Un site vitrine pour une entreprise.",
+        contexte: "Présenter les services et produits d'une société.",
+        technologies: "WordPress, Elementor.",
+        resultats: "Augmentation des demandes de contact.",
+        objectifs: "Offrir une vitrine moderne et responsive.",
+        images: "https://i.postimg.cc/bwmNYkmS/4.png",
+        videos: "https://drive.google.com/file/d/182SGwU2F2uCV8WMr1GDn5LNRk1vPWVHa/view?usp=drive_link",
+        testimonials: "Un site clair et efficace pour notre entreprise.",
+        client: "Client I",
+        date_realisation: new Date("2024-02-10"),
+        completed: true,
       },
 
       {
@@ -73,7 +73,7 @@ async function main() {
         technologies: "Vue.js, Node.js, PostgreSQL.",
         resultats: "Meilleure prise de décision grâce aux analyses.",
         objectifs: "Rendre les données plus compréhensibles via des graphiques.",
-        images:"https://i.postimg.cc/kgbGbTCF/dashboard-1.png",
+        images: "https://i.postimg.cc/NjdfGmdr/5.png",
         videos: "https://drive.google.com/file/d/13acT0rFfFwIIlpqf37GY9ZcqTFKpKHrB/view?usp=drive_link",
         testimonials: "Outil indispensable pour nos analyses marketing.",
         client: "Client G",
@@ -88,14 +88,14 @@ async function main() {
         technologies: "Next.js, Tailwind CSS.",
         resultats: "Visibilité accrue et plus de contacts professionnels.",
         objectifs: "Mettre en avant les créations avec un design attractif.",
-        images: "https://i.postimg.cc/0QZ01WKq/portfolio-cinema.png",
+        images: "https://i.postimg.cc/XYs7Nds5/6.png",
         videos: "https://drive.google.com/file/d/13acT0rFfFwIIlpqf37GY9ZcqTFKpKHrB/view?usp=drive_link",
         testimonials: "Très beau site, navigation fluide.",
         client: "Client F",
         date_realisation: new Date("2024-07-10"),
         completed: false,
       },
-    
+
 
     ],
   });
